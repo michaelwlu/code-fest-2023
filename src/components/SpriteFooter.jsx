4101f7ce1fd3682c133e659ce2241ce12ee29573
Michael W Lu <michaelwenlu@gmail.com>
2023-04-12 16:44:39 -0400

    Sprites

--- src/components/SpriteFooter.jsx
+++ src/components/SpriteFooter.jsx
@@ -19,23 +19,43 @@ export const SpriteFooter = () => {
         hello
       </div> */}
       {personOne && (
-        <div style={{ position: 'fixed', bottom: 30, left: 30 }}>
+        <div
+          style={{
+            position: 'fixed',
+            bottom: 30,
+            left: 30,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
           <Image
             src={`/sprites/${personOne?.sprite}`}
             width="300"
             height="500"
           />
+          <h1 style={{ color: 'white' }}>{personOne.name}</h1>
         </div>
       )}
 
       {/* Second Person */}
       {personTwo && (
-        <div style={{ position: 'fixed', bottom: 30, right: 30 }}>
+        <div
+          style={{
+            position: 'fixed',
+            bottom: 30,
+            right: 30,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
           <Image
             src={`/sprites/${personTwo?.sprite}`}
             width="300"
             height="500"
           />
+          <h1 style={{ color: 'white' }}>{personTwo.name}</h1>
         </div>
       )}
     </>
