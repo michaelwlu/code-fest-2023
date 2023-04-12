9bf1bb22388638c8ce38e52866bb0581a92ad9dc
Michael W Lu <michaelwenlu@gmail.com>
2023-04-12 13:14:35 -0400

    Add lists

---
+++ src/components/lists.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+export const personalities = [
+  { name: 'Mahatma Gandhi' },
+  { name: 'William Shakespeare' },
+  { name: 'Kanye West' },
+  { name: 'Steve Harvey' },
+  { name: 'Beyonce' },
+  { name: 'Taylor Swift' },
+  { name: 'Gwyneth Paltrow' },
+  { name: 'Queen Elizabeth II' },
+  { name: 'Karl Marx' },
+  { name: 'Albert Einstein' },
+  { name: 'Martin Luther King, Jr.' },
+  { name: 'Eminem' },
+];
+
+export const topics = [
+  [
+    {
+      topic: 'Artificial Intelligence',
+      question:
+        'Should the government regulate the use of artificial intelligence?',
+    },
+    {
+      topic: 'Economy',
+      question: 'Is capitalism or socialism a better economic system?',
+    },
+    {
+      topic: 'Gun Control',
+      question: 'Is gun control necessary to reduce violent crime?',
+    },
+    {
+      topic: 'Social Media',
+      question: 'Is social media more harmful than helpful for society?',
+    },
+    {
+      topic: 'Climate Change',
+      question: 'Is climate change primarily caused by human activity?',
+    },
+    {
+      topic: 'Drinking Age',
+      question: 'Should the drinking age be lowered or raised?',
+    },
+    {
+      topic: 'Healthcare',
+      question:
+        'Should the government provide free healthcare for all citizens?',
+    },
+    {
+      topic: 'Wealth Limits',
+      question:
+        'Should there be limits on the amount of wealth an individual can accumulate?',
+    },
+    {
+      topic: 'Censorship',
+      question: 'Is censorship necessary to protect society?',
+    },
+    {
+      topic: 'Education',
+      question: 'Is homeschooling a better option than traditional schooling?',
+    },
+    {
+      topic: 'Free Will',
+      question: 'Is the concept of free will an illusion?',
+    },
+    {
+      topic: 'Animal Welfare',
+      question: 'Is it ethical to use animals for food production?',
+    },
+  ],
+];
